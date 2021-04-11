9fd975038caa213c3757c8a929633396b0d3bdda
Seth Mitchell <sayotte.mitchell@gmail.com>
2021-04-11 16:05:31 -0400

    Nav, Branding, Footer

--- src/App.js
+++ src/App.js
@@ -21,10 +21,13 @@ import Resources from "./components/Resources";
 import Contact from "./components/Contact";
 import Blog from "./components/Blog";
 import Home from "./components/Home";
+import Footer from "./components/Footer";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "shards-ui/dist/css/shards.min.css";
 import "./sass/App.scss";
+
+import { BsThreeDots } from "react-icons/bs";
 
 export default class App extends React.Component {
   constructor(props) {
@@ -59,59 +62,78 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <Router>
-        <Navbar type="light" theme="light" expand="md" className="navBar">
-          <NavbarBrand href="/">SM</NavbarBrand>
-          <NavbarToggler onClick={this.toggleNavbar} />
+      <div>
+        <Router>
+          <Navbar expand="md" className="navBar">
+            <NavbarBrand href="/" className="navBrand">
+              sethmitchell<span className="navBrandDot">dot</span>co
+            </NavbarBrand>
+            <NavbarToggler
+              onClick={this.toggleNavbar}
+              className="toggleContainer"
+            >
+              <span>
+                <BsThreeDots className="toggleIcon" />
+              </span>
+            </NavbarToggler>
+            <Collapse open={this.state.collapseOpen} navbar>
+              <Nav navbar className="ml-auto">
+                <Dropdown
+                  className="dropdown"
+                  open={this.state.dropdownOpen}
+                  toggle={this.toggleDropdown}
+                >
+                  <NavLink href="/blog" className="navLink" id="blogLink">
+                    Blog
+                  </NavLink>
+                  <DropdownToggle nav caret />
+                  <DropdownMenu>
+                    <DropdownItem>Investing</DropdownItem>
+                    <DropdownItem>Budgeting</DropdownItem>
+                    <DropdownItem>Personal Finance</DropdownItem>
+                    <DropdownItem>Credit Cards</DropdownItem>
+                    <DropdownItem>Reading</DropdownItem>
+                  </DropdownMenu>
+                </Dropdown>
+                <NavItem>
+                  <NavLink href="/resources" className="navLink">
+                    Resources
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/contact" className="navLink">
+                    Contact
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/about" className="navLink">
+                    About
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Navbar>
 
-          <Collapse open={this.state.collapseOpen} navbar>
-            <Nav navbar className="ml-auto">
-              <Dropdown
-                className="dropdown"
-                open={this.state.dropdownOpen}
-                toggle={this.toggleDropdown}
-              >
-                <NavLink href="/blog">Blog</NavLink>
-                <DropdownToggle nav caret />
-                <DropdownMenu>
-                  <DropdownItem>Investing</DropdownItem>
-                  <DropdownItem>Budgeting</DropdownItem>
-                  <DropdownItem>Personal Finance</DropdownItem>
-                  <DropdownItem>Credit Cards</DropdownItem>
-                  <DropdownItem>Reading</DropdownItem>
-                </DropdownMenu>
-              </Dropdown>
-              <NavItem>
-                <NavLink href="/resources">Resources</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/contact">Contact</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="/about">About</NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
-
-        <Switch>
-          <Route path="/blog">
-            <Blog />
-          </Route>
-          <Route path="/resources">
-            <Resources />
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
+          <Switch>
+            <Route path="/blog">
+              <Blog />
+            </Route>
+            <Route path="/resources">
+              <Resources />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
+        <Footer />
+      </div>
     );
   }
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footerContainer">
+      <div className="footerLeftColumn">
+        <p>Link 1</p>
+        <p>Link 2</p>
+        <p>Link 3</p>
+        <p id="copyright">
+          Copyright © 2021 Seth Mitchell. All Rights Reserved.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
