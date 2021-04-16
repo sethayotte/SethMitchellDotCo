9f0748423cf953c921db1afd1730c56b604136f1
Seth Mitchell <sayotte.mitchell@gmail.com>
2021-04-15 20:24:25 -0400

    Inter Upload

--- src/index.js
+++ src/index.js
@@ -3,6 +3,26 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+// Inter Font Import
+import "./fonts/Inter-Black.otf";
+import "./fonts/Inter-BlackItalic.otf";
+import "./fonts/Inter-Bold.otf";
+import "./fonts/Inter-BoldItalic.otf";
+import "./fonts/Inter-ExtraBold.otf";
+import "./fonts/Inter-ExtraBoldItalic.otf";
+import "./fonts/Inter-ExtraLight.otf";
+import "./fonts/Inter-ExtraLightItalic.otf";
+import "./fonts/Inter-Italic.otf";
+import "./fonts/Inter-Light.otf";
+import "./fonts/Inter-LightItalic.otf";
+import "./fonts/Inter-Medium.otf";
+import "./fonts/Inter-MediumItalic.otf";
+import "./fonts/Inter-Regular.otf";
+import "./fonts/Inter-SemiBold.otf";
+import "./fonts/Inter-SemiBoldItalic.otf";
+import "./fonts/Inter-Thin.otf";
+import "./fonts/Inter-ThinItalic.otf";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
