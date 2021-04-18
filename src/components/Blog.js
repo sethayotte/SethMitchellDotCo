344019f9d396d352a9fd76324a829dc819dc1c4a
Seth Mitchell <sayotte.mitchell@gmail.com>
2021-04-18 14:56:29 -0400

    Route Correction & Data in UI

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,19 @@
 import React from "react";
+import allPosts from "../allPosts.json";
 
 import { Badge } from "shards-react";
 
 const Blog = () => {
+  const fetchedPost = {};
+
+  allPosts.data.forEach((post) => {
+    fetchedPost.title = post.title;
+    fetchedPost.author = post.author;
+    fetchedPost.date = post.date;
+    fetchedPost.category = post.category;
+    fetchedPost.content = post.contents;
+    console.log(fetchedPost);
+  });
   return (
     <div id="allPostsPage">
       <h1 id="allPostsHeader">All Posts</h1>
@@ -24,7 +35,15 @@ const Blog = () => {
           Books
         </Badge>
       </div>
-      <div className="recentPostsContainer"></div>
+      <div className="recentPostsContainer">
+        {/* <div>
+          <h6>{fetchedPost.category}</h6>
+          <h4>{fetchedPost.title}</h4>
+          <h6>
+            by {fetchedPost.author}, on {fetchedPost.date}
+          </h6>
+        </div> */}
+      </div>
     </div>
   );
 };
