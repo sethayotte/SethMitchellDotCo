dcaf350d41ea31342b22bb61ee89e55008dd5620
Seth Mitchell <sayotte.mitchell@gmail.com>
2021-04-18 13:55:22 -0400

    Route Config Update

--- getPosts.js
+++ getPosts.js
@@ -3,7 +3,7 @@ const markdownJson = require("markdown-json");
 const settings = {
   name: "markdown-json",
   cwd: "./",
-  src: "posts/",
+  src: "posts",
   filePattern: "**/*.md",
   ignore: "*(icon|input)*",
   dist: "src/allPosts.json",
